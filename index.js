1f37fcf9249b63c4ddc6e58665fd3eda0676e740
bobertsworld <bobertsworld@gmail.com>
2022-10-23 20:46:03 -0700

    commit tree 2a0cc8930cceaacd164ed9159ca71298178e2d81

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return a + b;
+  }
+function subtract(a, b) {
+    return a - b;
+  }
+function multiply(a, b) {
+    return a * b;
+  }
+function divide(a, b) {
+    return a / b;
+  }
+let n = 5;
+function increment(n) {
+    return n + 1;
+}
+
+function decrement(n) {
+        return n - 1;
+}
+
+
+let n = 5;
+function makeInt(n) {
+parseInt(n)
+parseInt(n, 10)
+}
+function preserveDecimal(n) {
+parseFloat(n)
+parseFloat(n, 10)
+
+
+}
